5fd2700e31c7d501367df6d4fcc7c01571d4429a
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-06-02 23:36:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,3 +40,13 @@ def sum_array(array)
   end
   i
 end
+
+def add_s(array)
+  new_ar = []
+  array.each do |x|
+    new_ar << x + "s"
+    
+  end
+  new_ar[1] = new_ar[1].gsub("s", "")
+  new_ar
+end
